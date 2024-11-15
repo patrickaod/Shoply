96ffaf320f2312df1309ae5392cae3b064ce1788
Patrick O'Doherty <patricius.aod@gmail.com>
2024-11-15 14:50:38 +0000

    Register product model in admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Product
 
-# Register your models here.
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'asin',
+        'title',
+        'categoryName',
+        'price',
+        'stars',
+        'isBestSeller',
+        'imgUrl',
+    )
+
+    ordering = ('title',)
+    
+admin.site.register(Product, ProductAdmin)
