3068d349df1bf618bba2a7b9f9aacb040bd5ddcd
Patrick O'Doherty <patricius.aod@gmail.com>
2024-11-18 16:38:36 +0000

    Fix bug in database_url

--- shoply/settings.py
+++ shoply/settings.py
@@ -137,7 +137,7 @@ WSGI_APPLICATION = 'shoply.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if "DATABASE_URL" in os.environ():
+if "DATABASE_URL" in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
