16711039ac2353cd0f3e64607d916bb1801148f4
Patrick O'Doherty <patricius.aod@gmail.com>
2024-11-21 14:04:20 +0000

    Initialise user activity application
    - Create model to track user interaction

---
+++ user_activity/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
+
+class RecentlyViewedProduct(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    viewed_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-viewed_at']  # Latest views first
+
+    def __str__(self):
+        return f"{self.user.username} viewed {self.product.name}"
