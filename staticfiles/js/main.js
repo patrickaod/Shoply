51fc3bfcab0b206f9a70d7171e4e758374c49579
Patrick O'Doherty <patricius.aod@gmail.com>
2024-11-22 23:44:00 +0000

    Update staticfiles with changes

--- staticfiles/js/main.js
+++ staticfiles/js/main.js
@@ -1,6 +1,7 @@
+/* jshint esversion: 11, jquery: true */ 
 // Initialize Bootstrap/Popper Tooltips
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
